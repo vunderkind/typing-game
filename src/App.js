7ac5f1f9aad7adfcff207dc36b257987f19b7bae
mogwai <feelirabs@gmail.com>
2020-07-19 03:00:07 +0100

    One mighty commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import useGame from './useGame';
+
+
+function App() {
+  const {timeRemaining, textAreaRef, words, startGame, handleChange, start, count} = useGame()
+  return (
+      <div className="App">
+      <h1>How fast can you type?</h1>
+      <h2>Time remaining: {timeRemaining}</h2>
+      <textarea
+      ref={textAreaRef}
+      name="textarea"
+      value={words}
+      disabled={!startGame}
+      onChange={handleChange}/>
+      <br/>
+  <button onClick={start} disabled={startGame}>{startGame? "Running": "Start!"}</button>
+  <h2>{count==0? "Word count: 0": `You typed ${count} in one minute!`}</h2>
+      </div>
+    
+  )
+}
+
+
+export default App;
---
+++ src/useGame.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect, useRef} from 'react';
+export default function useGame(){
+    const START_TIME = 5
+  const [count, setCount] = useState(0);
+  const [timeRemaining, setTimeRemaining] = useState(START_TIME);
+  const [words, setWords] = useState("");
+  const [startGame, setStartGame] = useState(false);
+  const textAreaRef = useRef(null);
+  const handleChange = (event)=> {
+    const {value} = event.target
+    setWords(value)
+  }
+  console.log(textAreaRef)
+
+  function start(){
+      setTimeRemaining(START_TIME)
+      setStartGame(true)
+      setWords("")
+  }
+
+  function calculateWords(x){
+    let wordsArray = x.trim().split(" ");
+    let finalCount = wordsArray.filter(word=>word!="").length;
+    setCount(finalCount);
+  }
+
+  function endGame(){
+    setStartGame(false);
+    calculateWords(words);
+  }
+  useEffect(()=> {
+    if(!startGame) {
+      return
+    }
+    if(timeRemaining>0){
+      setTimeout(()=>{
+        setTimeRemaining(prevTime=>prevTime-1)
+      },1000)
+  } else {
+    endGame()
+  }
+
+  }, [timeRemaining, startGame])
+
+  useEffect(()=> {
+      if(startGame){
+        textAreaRef.current.focus()
+      }
+  }, [startGame])
+
+  return {timeRemaining, textAreaRef, words, startGame, handleChange, start, count}
+
+}
